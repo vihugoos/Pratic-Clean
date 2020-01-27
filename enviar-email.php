96d367244f4ab874f8f0618dd83656f42b46617b
Victor Hugo <victorhugoos@live.com>
2020-01-27 18:13:21 -0200

    Added all site files.

---
+++ enviar-email.php
@@ -0,0 +1,33 @@
+<?php
+// Recebendo dados do formulário
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['tel'];
+$message = $_POST['message'];
+$subject = "Contato Pratic Clean";
+
+$headers = "Content-Type: text/html; charset=utf-8\r\n";
+$headers .= "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+// Dados que serão enviados
+$corpo = "Informações sobre o Cliente: <br><br>";
+$corpo .= "Nome: " . $name . " <br>";
+$corpo .= "Telefone: " . $phone . " <br>";
+$corpo .= "Email: " . $email . " <br><br>";
+$corpo .= "Mensagem: " . $message . " <br>";
+
+// Email que receberá a mensagem (Não se esqueça de substituir)
+$email_to = 'victorhugoos@live.com';
+
+// Enviando email
+$status = mail($email_to, mb_encode_mimeheader($subject, "utf-8"), $corpo, $headers);
+
+if ($status):
+  // Enviada com sucesso
+  header('location:index.php?status=sucesso');
+else:
+  // Se der erro
+  header('location:index.php?status=erro');
+endif;
+?>
